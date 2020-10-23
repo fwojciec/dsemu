061f45b8bc825728a66826ba292a28f69bb45218
Filip Wojciechowski <fwojciec@gmail.com>
2020-10-23 23:37:33 +0200

    updated project description

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="dsemu",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/fwojciec/dsemu",
     license="MIT",
     author="Filip Wojciechowski",
     author_email="fwojciec@gmail.com",
     description="A wrapper around the datastore emulator instance for use in tests.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests",)),
     install_requires=[],
     classifiers=[
