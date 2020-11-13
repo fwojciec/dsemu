a508a913ce178b1cbd2e4ac89a9faeb80e2701c0
Filip Wojciechowski <fwojciec@gmail.com>
2020-11-13 20:19:46 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dsemu",
-    version="0.2.0",
+    version="0.2.1",
     url="https://github.com/fwojciec/dsemu",
     license="MIT",
     author="Filip Wojciechowski",
